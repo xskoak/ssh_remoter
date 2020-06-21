43aff0e96e58183573d9def5791c6fd9ebf389e8
Aksel Skogsberg <xskoak@gmail.com>
2020-06-21 09:54:57 +0200

    Cleaned up script to follow specifications

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
+"""This script runs an arbitrary command on an arbitrary host"""
 
 import argparse
 import paramiko
 
+
 def arg_parser():
-    global args
+    """This function parses command line arguments"""
+    global ARGS
     parser = argparse.ArgumentParser()
-    parser.add_argument("--user", "-u", dest="USERNAME", required=True, help="Username of target")
-    parser.add_argument("--host", required=True, dest="HOSTNAME", help="Target hostname or IP address")
+    parser.add_argument(
+        "--user", "-u", dest="USERNAME", required=True, help="Username of target"
+    )
+    parser.add_argument(
+        "--host", required=True, dest="HOSTNAME", help="Target hostname or IP address"
+    )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--keyfile", "-k")
     group.add_argument("--password")
     parser.add_argument("--port", "-p", required=False, dest="port", default=22)
     parser.add_argument("COMMAND", help="Command to run")
-    args = parser.parse_args()
+    ARGS = parser.parse_args()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     arg_parser()
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(args.HOSTNAME, port=args.port, username=args.USERNAME, password=args.password, key_filename=args.keyfile)
+    ssh.connect(
+        ARGS.HOSTNAME,
+        port=ARGS.port,
+        username=ARGS.USERNAME,
+        password=ARGS.password,
+        key_filename=ARGS.keyfile,
+    )
 
-    stdin, stdout, stderr = ssh.exec_command(args.COMMAND)
+    stdin, stdout, stderr = ssh.exec_command(ARGS.COMMAND)
     lines = stdout.readlines()
     print(lines)
